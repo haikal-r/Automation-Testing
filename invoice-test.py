493182d579949b6cbb86629791108e7a5cd41f6d
Haikal Ramadhan <haikalramadhan1015@gmail.com>
2023-12-20 19:13:20 +0700

    Feat: Implement checkout automation testing

--- invoice-test.py
+++ invoice-test.py
@@ -21,16 +21,15 @@ try:
     password_field = driver.find_element(By.XPATH, '//input[@name="password"]')
     login_button = driver.find_element(By.XPATH, '//button[@type="submit"]')
 
-
     username = "pembeli2"
     password = "pembeli2"
 
     username_field.send_keys(username)
-    time.sleep(3)
+    time.sleep(1)
     password_field.send_keys(password)
-    time.sleep(3)
+    time.sleep(1)
     login_button.click()
-    time.sleep(3)
+    time.sleep(1)
 
     cart_link = driver.find_element(By.XPATH, "//a[@href='../user/keranjang.php']")
 
@@ -42,13 +41,10 @@ try:
 
     # Click on the Checkout button
     checkout_button.click()
-    time.sleep(3)
 
-    driver.execute_script("window.scrollBy(0, 500);")
-    tombol_update = driver.find_element(By.XPATH, "//button[text()='Buat Pesanan']")
-    tombol_update.click()
-    time.sleep(5)
-    
+    pesanan_button = driver.find_element(By.XPATH, '//button[@type="submit"]')
+    pesanan_button.click()
+    time.sleep(3)
 
     alert = Alert(driver)
     alert.accept()
@@ -71,9 +67,9 @@ current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 with open('uji-fungsionalitas.txt', 'a') as file:
     if "<h1>Internal Server Error</h1>" in driver.page_source:
-        file.write(f"Fitur Checkout - Diuji pada: {current_datetime} - Status: Error - Internal Server Error\n")
+        file.write(f"Fitur Login - Diuji pada: {current_datetime} - Status: Error - Internal Server Error\n")
     else:
-        file.write(f"Fitur Checkout - Diuji pada: {current_datetime} - Status: {status}\n")
+        file.write(f"Fitur Login - Diuji pada: {current_datetime} - Status: {status}\n")
 
 
 
